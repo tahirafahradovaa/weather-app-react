a346fbd77f08830e705567e4236b5e58cf64ef5e
tahirafahradovaaa <tairafahrad@icloud.com>
2022-12-22 15:50:13 +0400

    weather app sample

---
+++ src/components/Input.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import "./Card.css";
+function Input() {
+  const [country, setCountry] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [search, setSearch] = useState("Baku");
+  const [name, setName] = useState("");
+
+  const getData = () => {
+    setIsLoading(true);
+    fetch(
+      `http://api.weatherapi.com/v1/current.json?key=399f0a506174442e9fc101923222212&q=${search}&aqi=no`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setCountry(data.current);
+        console.log(country);
+        setName(data);
+        setIsLoading(false);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const handleChange = () => {
+    // setSearch(userInput);
+    console.log(search);
+    getData();
+  };
+
+  return (
+    <div className="Card-container">
+      <div className="input">
+        <input
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+        />
+        <button onClick={handleChange}> Get Weather </button>
+      </div>
+      {isLoading ? (
+        <span className="loader"></span>
+      ) : (
+        <div className="Card">
+          <h2>{name?.location?.name}</h2>
+          <img src={country?.condition?.icon} />
+          <p> {country?.condition?.text}</p>
+          <div>
+            <h2>{country?.temp_c}&#8451;</h2>
+            <h3>{country?.is_day}&#8451;</h3>
+          </div>
+          <div className="info">
+            <p>Humidity: {country?.humidity}</p>
+            <p>Wind: {country?.wind_kph} kph</p>
+            <p>Pressure: {country?.pressure_in}</p>
+            <p>Feels Like: {country?.feelslike_c} &#8451;</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Input;
